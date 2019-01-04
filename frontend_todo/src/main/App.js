a910b6b36fcac1406219701f8b0ce8a028030f32
Kenji Yasumitsu <kenjiyasumitsu@gmail.com>
2019-01-04 09:27:29 -0200

    first commit

---
+++ frontend_todo/src/main/App.js
@@ -0,0 +1,12 @@
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../node_modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+const App = (props) => (
+    <div className='container'>
+        <h1>Teste</h1>
+    </div>
+)
+
+export default App
